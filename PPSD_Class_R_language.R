b1483fd17bc9050269d96f777e24ce8365577592
Abdul Samad <53661372+abdulsamad-optimisticstr@users.noreply.github.com>
2019-11-16 00:01:23 +0500

    basic

---
+++ PPSD_Class_R_language.R
@@ -0,0 +1,32 @@
+a<-4.5
+b<-2.0
+c<-a+b
+d<-a+c
+print("The value of c is:")
+print(c)
+print("the value of d is:")
+print(d)
+myStr1="HELLO BABE ???"
+print(myStr1)
+myStr2="HELLO BABY ???"
+print(myStr2)
+if(a<b){
+  print("a is less than b")
+}else {
+  print("b is less than a")
+}
+myArr1=c(10,20,31,a,b,c,d)
+print(myArr)
+myArr2=c(1,2,3,a,b,c,d)
+print(myArr)
+dArr=myArr1/myArr2
+print(dArr)
+mArr=myArr1*myArr2
+print(mArr)
+myArr3=c(10,20,31,a,b,c,d)
+print(myArr3)
+if(myArr3==myArr1 || myArr1==myArr2){
+  print("any two of the stgrings are equal")
+}
+#
+for(i in )
